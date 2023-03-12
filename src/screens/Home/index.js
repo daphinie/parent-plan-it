664a2829baa6b74c985198db95751637a9b63fb0
daphinie <67568792+daphinie@users.noreply.github.com>
2023-03-11 17:51:26 -0800

    styling cand components to home page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,26 @@
-import { Dimensions, View, Text, StyleSheet } from 'react-native';
+import { Dimensions, View, Text, StyleSheet, Pressable } from 'react-native';
 import React from 'react';
+import { useFonts } from 'expo-font';
 
 const SCREENHEIGHT = Dimensions.get('window').height;
 const SCREENWIDTH  = Dimensions.get('window').width;
 
 const Home = () => {
+  const [loaded] = useFonts({
+    PTSans: require('../../../assets/fonts/PTSans-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}> Welcome </Text>
+      <Text style={styles.header}>Goals</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>+</Text>
+      </Pressable>
+      <Text style={styles.header}>Current</Text>
     </View>
   )
 }
@@ -17,15 +29,28 @@ export default Home
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: 'black',
+      backgroundColor: '#fffffa',
       height: SCREENHEIGHT,
       width: SCREENWIDTH,
       padding: 20,
   },
 
   header: {
-    fontFamily: 'AlongSansExtraBold',
-    color: 'white',
+    fontFamily: 'PTSans',
+    color: '#49416D',
     fontSize: 30,
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: '#99AEDD'
+  },
+
+  buttonText: {
+    color: '#49416D'
   }
 })
