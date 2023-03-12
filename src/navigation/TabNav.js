0b69a9e82fb5fcc9a80244d2d4cfdabf1d1ebe6b
Tara Ubovic <taraubovic@dhcp-206-87-232-74.ubcsecure.wireless.ubc.ca>
2023-03-11 23:44:30 -0800

    tara slay

--- src/navigation/TabNav.js
+++ src/navigation/TabNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 import Home from '../screens/Home';
 import Resources from '../screens/Resources';
 import BasicInfo from '../screens/BasicInfo';
@@ -9,9 +10,45 @@ const tab = createBottomTabNavigator();
 function TabNav(){
     return (
         <tab.Navigator initialRouteName="home">
-            <tab.Screen name="Goals" component={Home}></tab.Screen>
-            <tab.Screen name="Resources" component={Resources}></tab.Screen>
-            <tab.Screen name="Profile" component={BasicInfo}></tab.Screen>
+            <tab.Screen 
+              name="Goals" 
+              component={Home} 
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Image ta
+                    source={require('../../assets/goaltab.png')} 
+                    style={{ width: size, height: size }} 
+                    resizeMode="contain" 
+                  />
+                )
+              }}
+            />
+            <tab.Screen 
+              name="Resources" 
+              component={Resources} 
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Image 
+                    source={require('../../assets/resources active.png')} 
+                    style={{ width: size, height: size }} 
+                    resizeMode="contain" 
+                  />
+                )
+              }}
+            />
+            <tab.Screen 
+              name="Profile" 
+              component={BasicInfo} 
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Image 
+                    source={require('../../assets/profile active.png')} 
+                    style={{ width: size, height: size }} 
+                    resizeMode="contain" 
+                  />
+                )
+              }}
+            />
         </tab.Navigator>
     )
 }
